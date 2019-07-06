daffbc63f903e071c4f9a946fcdb516c38da6b71
jiepengtan <jiepengtan@gmail.com>
2019-07-06 23:18:48 +0800

    [+] 修改Setup.sh 添加代码拷贝

--- Setup.sh
+++ Setup.sh
@@ -3,16 +3,18 @@ echo " ------------LockstepPlatform Setup -------------"
 dir="$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)"
 cd $dir
 pwd
-echo " ------------(1/5)Build LPEngine ...-------------"
-msbuild /property:Configuration=Debug /p:WarningLevel=0 /verbosity:minimal ../LockstepPlatform/LPEngine.sln
-echo " ------------(2/5)Build LockstepPlatform ...-------------"
-msbuild /property:Configuration=Debug /p:WarningLevel=0 /verbosity:minimal ../LockstepPlatform/LockstepPlatform.sln
+_count=5
+echo " ------------(1/$(_count))Build LPEngine ...-------------"
+_idx=$_idx + 1
+#msbuild /property:Configuration=Debug /p:WarningLevel=0 /verbosity:minimal ../LockstepPlatform/LPEngine.sln
+#echo " ------------(2/$(_count))Build LockstepPlatform ...-------------"
+#msbuild /property:Configuration=Debug /p:WarningLevel=0 /verbosity:minimal ../LockstepPlatform/LockstepPlatform.sln
 
-echo " ------------(3/5)Copy libs  ...-------------"
+echo " ------------(3/$(_count))Copy libs  ...-------------"
 mkdir -p Libs
 cp -rf ../LockstepPlatform/Libs/ ./Libs/
 
-echo " ------------(4/5)Copy Tools  ...-------------"
+echo " ------------(4/$(_count))Copy Tools  ...-------------"
 mkdir -p ./Tools/bin/
 mkdir -p ./Tools/Src/
 cp -rf ../LockstepPlatform/Tools/bin/ ./Tools/bin/
@@ -20,8 +22,17 @@ cp -rf ../LockstepPlatform/Tools/Config/ ./Tools/Config/
 cp -rf ../LockstepPlatform/Tools/Src/*ECS* ./Tools/Src/
 cp -rf ../LockstepPlatform/Tools/Build*.sh ./Tools/
 
-echo " ------------(5/5)Build LPGame ...-------------"
+echo " ------------(5/$(_count))Build LPGame ...-------------"
 msbuild /property:Configuration=Debug /p:WarningLevel=0 /verbosity:minimal ./LPGame.sln
+
+
+echo " ------------(6/$(_count))Prepare Client CopySources ...-------------"
+cd Src/Unity
+pwd
+./CopySources.sh
+
+echo " ------------(7/$(_count))Prepare Client GenConfig ...-------------"
+./GenConfig.sh
 
 echo "Setup done :)"
 sleep 3
